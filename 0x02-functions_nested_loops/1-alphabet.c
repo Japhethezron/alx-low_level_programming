380a9c928f1f1b9aee36c96cf5f94b88e608acd3
Aruegho Japhet <japhethez91@gmail.com>
2023-03-03 16:04:17 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print all alphabet in lowercase
+ * @: Print all alphabet in lowercase
+ *
+ * description: Print all alphabet
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+}
